20984d80dc517034e86ab7a1fe334b88d150cd3b
TatianaBrt <brt79@mail.ru>
2023-06-06 18:30:42 +0300

    localhost was changed

--- src/Stripe/CheckoutForm.js
+++ src/Stripe/CheckoutForm.js
@@ -27,7 +27,7 @@ const totalPrice=useSelector(getTotalPrice)
       try {
         const { id } = paymentMethod;
         const response = await axios.post(
-          "http://localhost:8080/stripe/charge",
+          "https://shop-bf6w.onrender.com/stripe/charge",
           {
             amount: totalPrice*100,
             id: id,
