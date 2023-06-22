f138029d15f9d8175a5198629b04ee3a035497dc
Tatiana Bryzgalova <115479875+TatianaBrt@users.noreply.github.com>
2023-06-22 15:57:19 +0300

    Update LogIn.js

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -2,19 +2,9 @@ import React  from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import '../App.css';
 
-import { useNavigate } from 'react-router-dom'
-import { Children } from 'types'
-
-
 const LogIn=()=>{
-   const Auth0ProviderWithNavigate = ({ children }: Children) => {
- const navigate = useNavigate()
-
-      const onRedirectCallback = (appState: { returnTo?: https://furniture-v1gs.onrender.com/log})=>{
-         navigate(appState?.returnTo || window.location.pathname)}
-
-         
-   
+      
+ 
  const { loginWithRedirect, isAuthenticated } = useAuth0();
     return(
         !isAuthenticated && (
